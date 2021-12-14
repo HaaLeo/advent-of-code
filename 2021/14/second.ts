9d5a833101be713aa148a56eff8c38bc71a6f64d
Leo Hanisch <leo.hanisch@siemens.com>
2021-12-14 14:09:02 +0100

    update second problem properly

--- 2021/14/second.ts
+++ 2021/14/second.ts
@@ -5,14 +5,7 @@ export const second = (input: Input): number => {
     const template = polymerTemplate.split('');
     let result = 0;
 
-    const countMap: { [key: string]: number } = {}
-    for (const itm of template) {
-        if (!(itm in countMap)) {
-            countMap[itm] = 0;
-        }
-        countMap[itm]++
-    }
-    const pairMap: { [key: string]: number } = {}
+    let pairMap: { [key: string]: number } = {}
 
     for (let p = 0; p < template.length - 1; p++) {
         const curr = template[p] + template[p + 1];
@@ -22,51 +15,35 @@ export const second = (input: Input): number => {
         pairMap[curr] += 1
     }
 
-    for (let i = 0; i < 3; i++) {
-        const pairEntries = [...Object.entries(pairMap)]
-        for (const entry of pairEntries) {
-            const [key, value] = entry;
-            if (key in rules) {
-                const [first, second] = key.split('')
-                const newFirstKey = first + rules[key]
-                const newSecondKey = rules[key] + second
-
-
-
-                if (!(newFirstKey in pairMap)) {
-                    pairMap[newFirstKey] = 0
-                }
-                if (!(newSecondKey in pairMap)) {
-                    pairMap[newSecondKey] = 0
-                }
-
-                if (!(rules[key] in countMap)) {
-                    countMap[rules[key]] = 1
-                    pairMap[key] -= 1
-                } else {
-                    countMap[rules[key]] += value
-                    pairMap[key] -= value
-                }
-                if (rules[key]==='S'){
-                    console.log(`[${i}] Add ${rules[key]} for key ${key}`)
-                    console.log(`Pair map count ${pairMap[key]}`)
-                 }
-                if (pairMap[key]<0){
-                    console.debug('obacht')
-                }
-                pairMap[newFirstKey] += 1
-                pairMap[newSecondKey] += 1
-                // Remove old pair
+    for (let i = 0; i < 40; i++) {
+        const pairs = [...Object.keys(pairMap)];
+        const newPairCounts: { [key: string]: number } = {};
+        for (const pair of pairs) {
+            if (!rules[pair]) {
+                newPairCounts[pair] = pairMap[pair];
+            } else {
+                const [first, second] = pair.split('');
+                const newFirstKey = first + rules[pair];
+                const newSecondKey = rules[pair] + second;
+                newPairCounts[newFirstKey] = (newPairCounts[newFirstKey] ?? 0) + pairMap[pair];
+                newPairCounts[newSecondKey] = (newPairCounts[newSecondKey] ?? 0) + pairMap[pair];
             }
         }
+        pairMap = newPairCounts;
     }
+    const counts: { [key: string]: number } = {};
+    counts[template[0]] = 1;
+    Object.keys(pairMap).forEach((pair) => {
+        const [_, second] = pair.split("");
+        if (counts[second]) {
+            counts[second] += pairMap[pair];
+        } else {
+            counts[second] = pairMap[pair];
+        }
+    });
 
-    console.debug(JSON.stringify(countMap))
+    const sorted = Object.values(counts).sort((a, b) => a - b);
 
-    const sorted = Object.entries(countMap).sort((a, b) => a[1] - b[1])
-    const first = sorted[sorted.length - 1][1]
-    const second = sorted[0][1]
-
-    result = first - second
+    result = sorted[sorted.length - 1] - sorted[0]
     return result;
 }
